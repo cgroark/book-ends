f07ab54c001ac9ac5f2c10fc589726ecfb5b1d5e
Colin Groark <cgroark@enviroissues.com>
2020-01-15 17:01:22 -0800

    building out edit form

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -13,7 +13,9 @@ class BookForm extends React.Component {
             authorlast: '', 
             title: '', 
             status: '', 
-            datedone: ''
+            rating: '',
+            datedone: '',
+            editing: false
         }
 
     }
@@ -33,9 +35,28 @@ class BookForm extends React.Component {
                 console.log('data', this.state.allData)
             })
     }
+    updateBook = (each, e) =>{
+        this.setState({editing: true})
+        console.log(each, e);
+        this.setState({
+            authorfirst: each.authorfirst, 
+            authorlast: each.authorlast,
+            title: each.title, 
+            status: each.status, 
+            rating: each.rating,
+            datedone: each.datedone
+        })
+        console.log(this.state.title)
+    }
     renderAllData(){
-        return this.state.allData.filter(one => one.username === this.state.username).map((each, index) => 
-            <tr key={each.title}><td>{each.title}</td><td>{each.authorfirst} {each.authorlast}</td><td>{each.status}</td><td>{each.datedone}</td></tr>
+        return this.state.allData.filter(one => one.username === this.state.username && one.title).map((each, index) => 
+            <tr key={each.title}><td>{each.title}</td><td>{each.authorfirst} {each.authorlast}</td><td>{each.status}</td><td>{each.datedone}</td><td>{each.rating}</td>
+            <td>
+            <label htmlFor="edit"></label>
+            <input type="submit" value="Update" id="edit" onClick={(e) => this.updateBook(each,e)}></input>
+            </td>
+            
+            </tr>
         )
     }
     handleChange = e => this.setState({
@@ -83,6 +104,43 @@ class BookForm extends React.Component {
             return response.json()  
         });
       }
+      handleSubmitEdit = event => {
+        const dataSend = {
+            authorfirst: this.state.authorfirst,
+            authorlast: this.state.authorlast,
+            username: this.state.username,
+            title: this.state.title,
+            rating: this.state.rating,
+            status: this.state.status,
+            datedone: this.state.datedone
+        }
+        console.log(dataSend)
+        event.preventDefault()
+        this.setState({
+            submitting: true
+        })
+        console.log('before fetch', this.state.submitting)
+        fetch('https://sheet.best/api/sheets/f1c6e2c7-2b3d-4f85-8e10-39c1cf415351', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'UPATCH',
+            body: JSON.stringify(dataSend)
+        }).then( (response) => {
+            this.setState({
+                submitting: false,
+                authorfirst: '', 
+                authorlast: '', 
+                title: '', 
+                status: 'select-status', 
+                datedone: ''
+            })
+            console.log('done with book')
+            this.getAllData();
+            return response.json()  
+        });
+      }
       render(){
         const { submitting, authorfirst, authorlast, title, status, datedone} = this.state;
         let welcomeContent;
@@ -91,6 +149,45 @@ class BookForm extends React.Component {
             <div>
                 <Row>
                     <Col md={8}>
+                        {this.state.editing &&
+                            <div className="editor">
+                                <h3>Make your edits</h3>
+                                    <form onSubmit={this.handleSubmitEdit} className={submitting ? 'loading' : 'submit-form'}>
+                                        Edit your new book
+                                        <p>
+                                            <label >Title:<br />
+                                            <input type="text" name='title' value={title} onChange={this.handleChange} />
+                                                </label>
+                                        </p>
+                                        <p>
+                                            <label >Author first name:<br />
+                                                <input type="text" name='authorfirst' value={authorfirst} onChange={this.handleChange} /> 
+                                            </label>
+                                        </p>
+                                        <p>
+                                            <label>Author last name: <br />
+                                                <input type="text" name="authorlast" value={authorlast}  onChange={this.handleChange} />
+                                            </label>
+                                        </p>
+                                        <p>
+                                            <label>Status: <br />
+                                                <select defaultValue={"select-status"} onChange={this.updateStatus}>>
+                                                    <option value="select-status" disabled>Select status</option>
+                                                    <option value="Finished">Finished</option>
+                                                    <option value="Currently-Reading">Currently reading</option>
+                                                    <option value="Want-to-read">Want to read</option>
+                                                </select>
+                                            </label>
+                                        </p>
+                                        {status === "Finished" &&
+                                            <label >Date finished:<br />
+                                                <input type="text" name='datedone' value={datedone} onChange={this.handleChange} /> 
+                                            </label>
+                                        }
+                                        <input type='submit' disabled={submitting} value={submitting ? 'Loading...' : 'Submit'}></input>
+                                    </form>  
+                            </div>
+                        }
                         <table className="book-table">
                             <thead>
                                 <tr>
@@ -98,6 +195,8 @@ class BookForm extends React.Component {
                                     <th>Author</th>
                                     <th>Status</th>
                                     <th>Date finished</th>
+                                    <th>Rating</th>
+                                    <th>Edit</th>
                                 </tr>
                             </thead>
                             <tbody>
